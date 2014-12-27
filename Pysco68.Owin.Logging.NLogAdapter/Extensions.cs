c38143b71df774010b83c59ead2db97b2835fe1c
yannic <yannic.staudt@ygraphix.net>
2014-12-27 10:12:54 +0100

    Adding the logger middleware

---
+++ Pysco68.Owin.Logging.NLogAdapter/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Logging;
+
+namespace Pysco68.Owin.Logging.NLogAdapter
+{
+    public static class NlogFactoryExtensions
+    {
+        /// <summary>
+        /// Set the logger factory for this app builder to NLogFactory
+        /// </summary>
+        /// <param name="app"></param>
+        public static void UseNLog(this IAppBuilder app)
+        {
+            app.SetLoggerFactory(new NLogFactory());
+        }
+
+
+    }
+}
---
+++ Pysco68.Owin.Logging.NLogAdapter/NLogAdapter.cs
@@ -0,0 +1,78 @@
+﻿namespace Pysco68.Owin.Logging.NLogAdapter
+{
+    using Microsoft.Owin.Logging;
+    using NLog;
+    using System;
+    using System.Diagnostics;
+
+    public class NLogFactory : ILoggerFactory
+    {
+        /// <summary>
+        /// Creates a new ILogger instance of the given name.
+        /// </summary>
+        /// <param name="name">The logger context name.</param>
+        /// <returns>A logger instance.</returns>
+        public Microsoft.Owin.Logging.ILogger Create(string name)
+        {
+            return new Logger(name);
+        }        
+
+        class Logger : Microsoft.Owin.Logging.ILogger
+        {
+            readonly NLog.Logger _logger;
+           
+            internal Logger(string name)
+            {
+                _logger = LogManager.GetLogger(name);
+            }
+
+            public bool WriteCore(TraceEventType eventType, int eventId, object state, Exception exception, Func<object, Exception, string> formatter)
+            {
+                var level = GetLogLevel(eventType);
+                
+                // According to docs http://katanaproject.codeplex.com/SourceControl/latest#src/Microsoft.Owin/Logging/ILogger.cs
+                // "To check IsEnabled call WriteCore with only TraceEventType and check the return value, no event will be written."
+                if (state == null)
+                {
+                    return _logger.IsEnabled(level);
+                }
+                if (!_logger.IsEnabled(level))
+                {
+                    return false;
+                }
+
+                _logger.Log(level, formatter(state, exception), exception);
+                return true;
+            }
+
+            static LogLevel GetLogLevel(TraceEventType traceEventType)
+            {
+                switch (traceEventType)
+                {
+                    case TraceEventType.Critical:
+                        return LogLevel.Fatal;
+                    case TraceEventType.Error:
+                        return LogLevel.Error;
+                    case TraceEventType.Warning:
+                        return LogLevel.Warn;
+                    case TraceEventType.Information:
+                        return LogLevel.Info;
+                    case TraceEventType.Verbose:
+                        return LogLevel.Trace;
+                    case TraceEventType.Start:
+                        return LogLevel.Debug;
+                    case TraceEventType.Stop:
+                        return LogLevel.Debug;
+                    case TraceEventType.Suspend:
+                        return LogLevel.Debug;
+                    case TraceEventType.Resume:
+                        return LogLevel.Debug;
+                    case TraceEventType.Transfer:
+                        return LogLevel.Debug;
+                    default:
+                        throw new ArgumentOutOfRangeException("traceEventType");
+                }
+            }
+        }
+    }
+}
