62a209c6fe78d879c00423b0ace20003ee837c69
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-03-10 18:28:32 +0100

    Update name

--- NLog.Owin.Logging.Tests/Properties/AssemblyInfo.cs
+++ NLog.Owin.Logging.Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pysco68.Owin.Logging.NLogAdapter.Tests")]
+[assembly: AssemblyProduct("NLog.Owin.Logging.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
