48e86e028eab07562520b63854cf65f45d0e8d8f
Yannic Staudt <yannic.staudt@staudt-engineering.com>
2016-03-10 15:33:24 +0100

    Implemented test cases for the LogAdapter

---
+++ NLog.Owin.Logging.Tests/LogAdapterTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using Microsoft.Owin.Testing;
+using Owin;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin;
+using NLog.Config;
+using NLog.Targets;
+
+namespace NLog.Owin.Logging.Tests
+{
+    public class StartupStandard
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNLog();
+            app.Use<TestMiddleware>(app);
+        }
+    }
+
+    [TestFixture]
+    public class OwinLoggerTest
+    {
+        private TestServer _server;
+        private DebugTarget _debugTarget;
+
+
+        [OneTimeSetUp]
+        public void FixtureInit()
+        {
+            _server = TestServer.Create<StartupStandard>();
+
+            // setup the debug target
+            this._debugTarget = new DebugTarget();
+            this._debugTarget.Layout = "${level} ${message}";
+
+            SimpleConfigurator.ConfigureForTargetLogging(_debugTarget, LogLevel.Trace);
+        }
+
+        [OneTimeTearDown]
+        public void FixtureDispose()
+        {
+            _server.Dispose();
+        }
+
+        [TestCase("/critical", "critical", "Fatal")]
+        [TestCase("/error", "error", "Error")]
+        [TestCase("/warning", "warning", "Warn")]
+        [TestCase("/information", "information", "Info")]
+        [TestCase("/verbose", "verbose", "Trace")]
+        public async Task TestLogmessages(string route, string expectedMessageEnd, string expectedLogLevel)
+        {
+            var result = await _server.CreateRequest(route).GetAsync();
+            result.EnsureSuccessStatusCode();
+
+            // note: the log messages end with watherver was logged thus we want to check for that
+            Assert.That(_debugTarget.LastMessage, Does.StartWith(expectedLogLevel));
+            Assert.That(_debugTarget.LastMessage, Does.EndWith(expectedMessageEnd));
+        }
+    }
+}
---
+++ NLog.Owin.Logging.Tests/OwinTestApp.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Logging;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Owin.Logging.Tests
+{
+    /// <summary>
+    /// This owin middleware does log messages matching the called route:
+    /// 
+    /// /critical       => "critical"
+    /// /error          => "error"
+    /// /warning        => "warning"
+    /// /information    => "information"
+    /// /verbose        => "verbose"
+    /// 
+    /// Calling these routes allows us to check the function of the 
+    /// registered OWIN logger factory
+    /// </summary>  
+    public class TestMiddleware : OwinMiddleware
+    {
+        private readonly Microsoft.Owin.Logging.ILogger _logger;
+
+        public TestMiddleware(OwinMiddleware next, IAppBuilder app) : base(next)
+        {
+            _logger = app.CreateLogger<TestMiddleware>();
+        }
+
+        public override async Task Invoke(IOwinContext context)
+        {
+            switch (context.Request.Path.Value)
+            {
+                case "/critical":
+                    _logger.WriteCritical("critical"); ;
+                    break;
+                case "/error":
+                    _logger.WriteError("error");
+                    break;
+                case "/warning":
+                    _logger.WriteWarning("warning");
+                    break;
+                case "/information":
+                    _logger.WriteInformation("information");
+                    break;
+                case "/verbose":
+                    _logger.WriteVerbose("verbose");
+                    break;
+            }
+
+            await context.Response.WriteAsync("Cool!");
+        }
+    }
+
+}
