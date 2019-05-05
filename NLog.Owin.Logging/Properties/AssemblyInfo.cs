53d3899f9cdddde3e50adf633cf95b36856dfa77
Yannic Staudt <yannic@staudt-engineering.com>
2019-05-05 14:21:15 +0200

    Updated solution and csproj files to VS2017 modern format

--- NLog.Owin.Logging/Properties/AssemblyInfo.cs
+++ NLog.Owin.Logging/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NLog.Owin.Logging")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("NLog")]
-[assembly: AssemblyProduct("NLog.Owin.Logging")]
-[assembly: AssemblyCopyright("Copyright © Yannic Staudt & NLog 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,17 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly:Guid("84DC71BF-9C34-4801-ADA5-2E01BB0B63B2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-
-//Only change AssemblyVersion with major releases!
-[assembly: AssemblyVersion("1.0.0.0")]
-//always update AssemblyFileVersion with higher number!
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0")]
